83db5c5894031fe413264f875044b9b7d5d263af
Ben Blakemore <blakemoreb1000@gmail.com>
2023-02-12 12:36:25 +0000

    Reverted back to flashing on-board LED. Used PuTTY session to show serial communication via USB of when led was blinking

--- pico/pico_workspace/blink_led/main.c
+++ pico/pico_workspace/blink_led/main.c
@@ -1,35 +1,25 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/uart.h"
-
-/// \tag::hello_uart[]
-
-#define UART_ID uart0
-#define BAUD_RATE 115200
-
-// We are using pins 0 and 1, but see the GPIO function select table in the
-// datasheet for information on which other pins can be used.
-#define UART_TX_PIN 0
-#define UART_RX_PIN 1
 
 int main() {
-    // Set up our UART with the required speed.
-    uart_init(UART_ID, BAUD_RATE);
 
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+    const uint led_pin = 25;
 
-    // Use some the various UART functions to send out data
-    // In a default system, printf will also output via the default UART
+    // Initialize LED pin
+    gpio_init(led_pin);
+    gpio_set_dir(led_pin, GPIO_OUT);
 
-    // Send out a character without any conversions
-    uart_putc_raw(UART_ID, 'A');
+    // Initialize chosen serial port
+    stdio_init_all();
 
-    // Send out a character but do CR/LF conversions
-    uart_putc(UART_ID, 'B');
+    // Loop forever
+    while (true) {
 
-    // Send out a string, with CR/LF conversions
-    uart_puts(UART_ID, " Hello, UART!\n");
+        // Blink LED
+        printf("Blinking!\r\n");
+        gpio_put(led_pin, true);
+        sleep_ms(1000);
+        gpio_put(led_pin, false);
+        sleep_ms(1000);
+    }
 }
